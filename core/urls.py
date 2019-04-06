15f05387a21e539c69480bf39c7f3daa3000bee1
diegopmayer <dpmayer@hotmail.com>
2019-04-06 19:03:48 -0300

    changed update

--- core/urls.py
+++ core/urls.py
@@ -26,23 +26,23 @@ urlpatterns = [
     #url(r'^pessoas-update/(?P<id>\d+)/$', pessoas_update, 
     # name='core_pessoas_update'), VERSÃO ANTIGA DO DJANGO - 
     # VERSÃO NOVA ABAIXO (PATH)
-    path('^pessoas-update/<int:id>/', pessoas_update, name='core_pessoas_update'),
+    path('pessoas-update/<int:id>/', pessoas_update, name='core_pessoas_update'),
 
     url(r'^veiculos/$', lista_veiculos, name='core_lista_veiculos'),
     url(r'^veiculos-add/$', veiculos_add, name='core_veiculos_add'),
-    url(r'^veiculos-update/(?P<id>\d+)/$', veiculos_update, name='core_veiculos_update'),
+    path('veiculos-update/<int:id>/', veiculos_update, name='core_veiculos_update'),
 
     url(r'^mov-rot-list/$', lista_movrotativos, name='core_lista_movrotativos'),
     url(r'^mov-rot-add/$', mov_rot_add, name='core_mov_rot_add'),
-    path('mov-rot-update/<int:id>/', mov_rot_update, name='mov_rot_update'),
+    path('mov-rot-update/<int:id>/', mov_rot_update, name='core_mov_rot_update'),
 
     url(r'^mov-mensalistas/$', lista_movmensalista, 
         name='core_lista_movmensalista'),
     url(r'^mov-mensalistas-add/$', mov_mensalista_add, 
         name='core_mov_mensalista_add'),
-    path('mov-mensalista-update/<int:id>/', mov_mensalista_update, name='mov_mensalista_update'),
+    path('mov-mensalista-update/<int:id>/', mov_mensalista_update, name='core_mov_mensalista_update'),
 
     url(r'^mensalistas/$', lista_mensalista, name='core_lista_mensalista'),
     url(r'^mensalistas-add/$', mensalista_add, name='core_mensalista_add'),
-    path('mensalista-update/<int:id>/', mensalista_update, name='mensalista_update'),
+    path('mensalista-update/<int:id>/', mensalista_update, name='core_mensalista_update'),
 ]
